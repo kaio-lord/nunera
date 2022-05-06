67c8c41c4a977e8313037d5eaf64679a435b2c19
nunera <michaelnkryvonis@gmail.com>
2022-05-06 09:27:09 -0500

    it feels good when you know you're down, super dope homeboy from the oak town and i'm known. As such, and this is a beat, uh, can't touch.

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-var form = document.getElementById('srchbutton');
-var input = document.getElementById('srchinput');
-
-form.addEventListener('click', async event => {
-    event.preventDefault();
-    window.navigator.serviceWorker.register('./sw.js', {
-        scope: __uv$config.prefix
-    }).then(() => {
-        let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-
-
-        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-    });
-});
-
-function isUrl(val = '') {
-    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
-    return false;
-};
+(async() => {
+  await import('./server.mjs').then(e=>e.default({
+    cookie: false, // Cookie Authentication
+    primaryProxy: 'ultraviolet', // Main Proxy on Enter
+    googleBlock: true, // Block Google's Bots
+    cors: true, // Allow Cross-Origin Requests
+    config: {
+      port: 8080, // Port to host site on (Default: 8080)
+      gamePort: 3000, // Port for games (Default: 3000) (NOT IMPLEMENTED)
+    }
+  }));
+})();
