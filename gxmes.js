4577312fdf2bbe20f1a32051e6cf32efbf92d39f
nunera <michaelnkryvonis@gmail.com>
2022-05-17 13:43:32 -0500

    guac (amole)

--- gxmes.js
+++
@@ -1,7 +0,0 @@
-const elemsrendered = () => {
-  document.querySelectorAll("h2").forEach(el => el.addEventListener("click",function() {
-    alert("e")
-  location.href = "../play/?" + this.textContent
-}))
-
-}
